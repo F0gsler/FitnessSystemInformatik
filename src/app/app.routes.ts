4f870478624347ae48afc7d9164f32ef1ce8c298
Lukas <haugstadlukas@gmail.com>
2026-04-30 14:37:44 +0200

    admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Admin } from '../components/admin/admin';  
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: 'admin', component: Admin }
+];
