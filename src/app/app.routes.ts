45afb2c294725d65d49c1f55763afb61ec0dd4ed
AndreasLorenzen <andreas.141106@gmail.com>
2026-04-30 14:38:09 +0200

    Add booking route to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { BrugerOprettelse } from '../components/bruger-oprettelse/bruger-oprettelse'
+import { Booking } from '../components/booking/booking';
 
 export const routes: Routes = [
-    {path: 'test', component: BrugerOprettelse}
+    {path: 'test', component: BrugerOprettelse,
+     path: 'booking', component: Booking
+    }
 ];
