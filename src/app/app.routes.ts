2d5ae30314b24c71f5fad0608879dae54e64a975
Marius Fogelgren <marius.ulslev.fogelgren@gmail.com>
2026-04-30 14:30:34 +0200

    added brugeroprettelse

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BrugerOprettelse } from '../components/bruger-oprettelse/bruger-oprettelse'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'test', component: BrugerOprettelse}
+];
